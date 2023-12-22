497e7334331245925f5de11181d66f6ca7abef88
Oluwatobi Ogunronbi <bigtobi@Oluwatobis-MacBook-Pro.local>
2023-12-21 23:18:14 -0600

    11th commit

--- pages/api/message.js
+++ pages/api/message.js
@@ -1,13 +1,18 @@
-import {
-    Configuration,
-    OpenAIApi
-} from "openai";
+// import {
+//    Configuration,
+//    OpenAIApi
+// } from "openai";
 
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+// const configuration = new Configuration({
+//    apiKey: process.env.OPENAI_API_KEY,
+// });
+// const openAI = new OpenAIApi(configuration);
+
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
-
-const openAI = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
 
