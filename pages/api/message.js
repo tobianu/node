378c74bd8f677bb4f255869ecd5fca9f40eee831
Oluwatobi Ogunronbi <bigtobi@Oluwatobis-MacBook-Pro.local>
2023-12-22 00:10:03 -0600

    V13.3 commit: completion method to include .chat.

--- pages/api/message.js
+++ pages/api/message.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
     } else {
 
         try {
-            const completion = await openai.completions.create({
+            const completion = await openai.chat.completions.create({
                 model: "gpt-4-1106-preview", // required
                 messages: [{
                     role: "user",
