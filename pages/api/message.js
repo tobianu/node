1798f6674c060f62ce19404dea4a0dbb462239a9
Oluwatobi Ogunronbi <bigtobi@Oluwatobis-MacBook-Pro.local>
2023-12-21 21:23:17 -0600

    7th commit

--- pages/api/message.js
+++ pages/api/message.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
         try {
             const completion = await openAI.createCompletion({
-                model: "gpt-3.5-turbo", // required
+                model: "text-davinci-003", // required
                 prompt: req.body.Body, // completion based on this
                 temperature: 0.6, //
                 n: 1,
