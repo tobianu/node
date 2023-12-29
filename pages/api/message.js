4d2119027842bca038285b2ebc7db5367e20c177
Oluwatobi Ogunronbi <bigtobi@Oluwatobis-MacBook-Pro.local>
2023-12-28 22:35:07 -0600

    V13.8 commit: Changed model to gpt-4-vision-preview

--- pages/api/message.js
+++ pages/api/message.js
@@ -17,7 +17,7 @@ export default async function handler(req, res) {
 
     try {
         const completion = await openai.chat.completions.create({
-            model: "gpt-3.5-turbo",
+            model: "gpt-4-vision-preview",
             messages: [
                 { role: "system", content: "You are a helpful assistant." },
                 { role: "user", content: sentMessage }
